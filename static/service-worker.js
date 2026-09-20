2ed3fc64353be84ef1a85270df3e4943a589970f
primejarvis4 <primejarvis4@gmail.com>
2026-09-20 08:21:00 +0545

    Add Studymate PWA support

---
+++ static/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "studymate-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/static/index.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+
+
